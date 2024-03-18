78389e3b5f307f0cb6b31d302cb55f8acf587f0b
Grael Camila <camigrael@gmail.com>
2024-03-17 23:26:08 -0300

    guia 2

--- Intro Java/src/guia1/Ejercicio1.java
+++ Intro Java/src/guia1/Ejercicio1.java
@@ -1,9 +1,9 @@
 
-package dos;
+package guia1;
 
 import java.util.Scanner;
 
-public class Dos {
+public class Ejercicio1 {
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
