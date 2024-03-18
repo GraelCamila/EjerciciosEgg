50cf6db45a06128a5f52a2739454c49b9d595ee6
Grael Camila <camigrael@gmail.com>
2024-03-17 22:43:04 -0300

    java

---
+++ dos/src/dos/Dos.java
@@ -0,0 +1,19 @@
+
+package dos;
+
+import java.util.Scanner;
+
+public class Dos {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Ingrese dos números enteros");
+        int num1 = leer.nextInt();
+        int num2 = leer.nextInt();
+       
+        int res = num1 + num2;
+        
+        System.out.println("El resultado de la suma de los números ingresados es: " + res);
+    }
+    
+}
